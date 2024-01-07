64bdd1fcf1ec2450f8a6293945c86e87b3336b94
power-minu <mw0677@naver.com>
2024-01-08 02:15:39 +0900

    feat: 게시글 작성폼, 글 작성 처리, 게시글 리스트

---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,21 @@
+package com.study.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+//entity는 디비에서 table이라고 생각하면 됨.
+@Entity
+@Getter
+@Setter
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String content;
+}
