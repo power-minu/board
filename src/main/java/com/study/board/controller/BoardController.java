d2ebb06ea0b046bb631c1ce82066ab1cce55efbd
power-minu <mw0677@naver.com>
2024-01-08 21:15:29 +0900

    feat: 게시글 상세 페이지

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -37,4 +37,11 @@ public class BoardController {
 
         return "boardlist";
     }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id) {
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
 }
